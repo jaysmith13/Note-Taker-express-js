26853b4294bbd12a4731dec2fa85599f225c0ba0
jaysmith13 <janeasmith13@gmail.com>
2022-08-01 22:18:07 -0600

    moving routes over

---
+++ Routes/apiRoutes.js/index.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+const saveData = require('../db/saveData');
+const { route } = require('../htmlRoutes.js');
+
+//get request
+
+router.get('/notes', function (req, res){
+    saveData
+    .retrieveNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+
+});
+
+//post request
+
+router.post('/notes', (req, res) => {
+    saveData
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch(err => res.status(500).json(err));
+});
+
+//delete request
+
+
+router.delete('/notes/:id', function(req, res){
+    saveData
+    .deleteNote(req.params.id)
+    .then(() => res.json ({ok : true}))
+    .catch(err => res.status(500).json(err));
+
+});
+
+module.exports = router;
