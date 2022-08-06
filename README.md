bd138f526b5239df06511554d5821472bded82ce
jaysmith13 <janeasmith13@gmail.com>
2022-08-06 12:22:08 -0600

    saveData update
    add note error fix

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Note taker is an application designed to store and save lists and ideas.
 ## Screenshot
 ![Note Taker](https://user-images.githubusercontent.com/99004555/182289501-8517da67-94ee-490f-b50a-ba51b414661f.png)
 
-
 ## Credits
 
 I used source code provided by the University and Sparc Plus Tech express.js demo
--- db/saveData.js
+++ db/saveData.js
@@ -1,8 +1,14 @@
 const util = require('utils');
 const fs = require('fs');
+
+
 const { notStrictEqual } = require('assert');
 const { title } = require('process');
 const { text } = require('body-parser');
+
+const writeNote = util.promisify(fs.readFile);
+const readNote = util.promisify(fs.writeFile);
+
 
 
 
@@ -32,7 +38,12 @@ if (!title || !text){
     throw new Error('Please do not leave blank');
 }
 
+const newNote = {title, text, id};
 
+return this.retrieveNotes()
+.then(updatedNotes => this.write(updatedNotes))
+.then(notes =>[...notes,newNote])
+.then(() => newNote);
 
 
 
