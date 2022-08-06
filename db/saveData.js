70f1a2d7b77097115e1af72cf0d81109bee4e844
jaysmith13 <janeasmith13@gmail.com>
2022-08-06 12:05:43 -0600

    Update saveData.js
    error fix

--- db/saveData.js
+++ db/saveData.js
@@ -1,6 +1,8 @@
 const util = require('utils');
 const fs = require('fs');
 const { notStrictEqual } = require('assert');
+const { title } = require('process');
+const { text } = require('body-parser');
 
 
 
@@ -22,18 +24,24 @@ const { notStrictEqual } = require('assert');
 
 
 
+
+addNote(note) 
+const{  title, text
+} =note;
+if (!title || !text){
+    throw new Error('Please do not leave blank');
+}
 
 
 
 
 
 //Delete Note
-deleteNote(id){
+deleteNote(id) 
+{
+    return this.retrieveNotes()
     .then(note => notes.filter(note => note.id !== parseInt(id)))
     .then(filteredNotes => this.write(filteredNotes));
 }
-
-
-
 
 module.exports = new Save();
